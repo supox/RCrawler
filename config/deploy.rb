f47c978d0e7e9451be45f8fa471f0db2b6cca685
supox <supox0@walla.co.il>
2014-02-08 20:04:10 +0200

    Added files for cap

---
+++ config/deploy.rb
@@ -0,0 +1,93 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'rap_crawler'
+set :repo_url, 'git@github.com:supox/RapCrawler.git'
+set :user, 'deploy'
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, "/home/#{ fetch(:user) }/apps/#{ fetch(:application) }"
+
+# Default value for :scm is :git
+set :scm, :git
+
+# rvm dirty trick
+# set :default_env, { rvm_bin_path: '~/.rvm/bin' }
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# files we want symlinking to specific entries in shared.
+set :linked_files, %w{config/database.yml config/application.yml config/app_environment_variables.rb config/nginx.conf config/unicorn_init.sh}
+
+# dirs we want symlinking to shared
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# what specs should be run before deployment is allowed to
+# continue, see lib/capistrano/tasks/run_tests.cap
+set :tests, ["spec"]
+
+# which config files should be copied by deploy:setup_config
+# see documentation in lib/capistrano/tasks/setup_config.cap
+# for details of operations
+set(:config_files, %w(
+  nginx.conf
+  application.yml
+  database.example.yml
+  log_rotation
+  monit
+  unicorn.rb
+  unicorn_init.sh
+))
+
+# which config files should be made executable after copying
+# by deploy:setup_config
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
+
+# files which need to be symlinked to other parts of the
+# filesystem. For example nginx virtualhosts, log rotation
+# init scripts etc.
+set(:symlinks, [
+  {
+    source: "nginx.conf",
+    link: "/etc/nginx/sites-enabled/#{fetch(:full_app_name)}"
+  },
+  {
+    source: "unicorn_init.sh",
+    link: "/etc/init.d/unicorn_#{fetch(:full_app_name)}"
+  },
+  {
+    source: "log_rotation",
+   link: "/etc/logrotate.d/#{fetch(:full_app_name)}"
+  },
+  {
+    source: "monit",
+    link: "/etc/monit/conf.d/#{fetch(:full_app_name)}.conf"
+  }
+])
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+  # make sure we're deploying what we think we're deploying
+  before :deploy, "deploy:check_revision"
+  # compile assets locally then rsync
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
+  after :finishing, 'deploy:restart_unicorn'
+
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '10.0.0.9', user: fetch(:user), roles: %w{web app db}
